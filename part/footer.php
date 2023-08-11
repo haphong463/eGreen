d5990bb4ce83d23d58441db4d9ccb5eb0bca2fb5
Bo <truongtanphat190703@gmail.com>
2023-08-11 21:00:18 +0700

    Update footer.php

--- part/footer.php
+++ part/footer.php
@@ -1,8 +1,8 @@
 <!-- footer -->
 <!--  data-aos="fade-up" data-aos-duration="1500" -->
 <footer id="footer">
-    <h1 class="text-center">galaxy</h1>
-    <p class="text-center">mlem lêm ẹnib faibibc ybiec yiaea biy fuwwg w f uyrsgf iyr ujdhfignuvh srifguckjhs irufkgc jhvi7rsukfjch</p>
+    <h1 class="text-center">TEAM TREE</h1>
+    <p class="text-center">Trees are our life, let's join hands to protect all trees in the world.</p>
     <div class="icons text-center">
         <i class="bx bxl-twitter"></i>
         <i class="bx bxl-facebook"></i>
@@ -10,9 +10,7 @@
         <i class="bx bxl-skype"></i>
         <i class="bx bxl-instagram"></i>
     </div>
-    <div class="copyright text-center">
-        &copy;Copyright <strong>.All rights Reserved</strong>
-    </div>
+
     <div class="credite text-center">
         Designed By <a href="#"><span>Bobu Coding</span></a>
     </div>
