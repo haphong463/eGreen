e6136300c66d39fd94629bb6859038467681d055
Bo <truongtanphat190703@gmail.com>
2023-08-11 14:35:26 +0700

    a

--- userEdit.php
+++ userEdit.php
@@ -10,7 +10,9 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
     <link rel="stylesheet" href="css/contact.css">
-
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+    
     <!-- BStrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
@@ -18,6 +20,9 @@
 </head>
 
 <body>
+<?php
+    include('part/header.php');
+    ?>
     <div class="wrapper">
         <form action="">
             <h1>Change Information</h1>
