2392c052a7871de060a544215c5d95902696d761
Biden678 <davidphuc91@gmail.com>
2023-08-11 12:09:10 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,153 +1,173 @@
 <?php
-    session_start();
-   include_once('db/dbhelper.php');
-   require_once 'vendor/autoload.php'; // Import thư viện firebase/php-jwt
-   
-   use \Firebase\JWT\JWT;
+session_start();
+require_once('db/dbhelper.php');
+require_once 'vendor/autoload.php';
 
 
-    // Tạo mã token
-    function createToken($secretKey, $expirationTimeMinutes) {
-        // Lấy thời gian hiện tại
-        $currentTime = time();
-        // Tính thời gian hết hạn
-        $expirationTime = $currentTime + ($expirationTimeMinutes * 60);
-    
-        // Tạo payload (dữ liệu chứa trong token)
-        $payload = array(
-            'user_id' => '1234567890',  // Thông tin cần lưu trữ trong token
-            'exp' => $expirationTime  // Thời gian hết hạn của token
-        );
-    
-        // Tạo mã token với secret key
-        $jwtBuilder = new JWT();
-        $token = $jwtBuilder->encode($payload, $secretKey, 'HS256');
-    
-        return $token;
+define("APPPATH", "phpMailer/");
+include APPPATH . "PHPMailer.php";
+include APPPATH . "Exception.php";
+include APPPATH . "OAuth.php";
+include APPPATH . "POP3.php";
+include APPPATH . "SMTP.php";
+
+use PHPMAILER\PHPMailer\PHPMailer;
+use PHPMAILER\PHPMailer\Exception;
+use PHPMAILER\PHPMailer\SMTP;
+
+function generateVerificationCode($length = 6)
+{
+    $characters = '0123456789';
+    $code = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $code .= $characters[rand(0, strlen($characters) - 1)];
     }
-    // token
-    
-    if (isset($_POST['create'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $fullname = $_POST['fullname'];
-        $password = $_POST['password'];
-        $cpassword = $_POST['cpassword'];
-        $err = [];
- 
 
-    
-        if (empty($username)) {
-            $err['username'] = 'username must not be empty';
-        } 
-        // else {
-        //     if (!preg_match("/^[a-zA-Z]{2,10}$/", $fname)) {
-        //         $err['fname'] = 'Firstname must be from 3 to 10 characters and not a number';
-        //     }
-        // }
-        if(empty($email)){
-            $err['email']='email must be not empty';
-            }else{
-                if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                    $err['email'] ='Invalid email';
-                }else {
-                    //kiem tra email có tồn tại trong db chưa
-                    $sql1 = "SELECT * FROM users WHERE email='$email'";
-                    $checkRegister = executeSingleResult($sql1);
-                    if ($checkRegister) {
-                        $err['email'] = 'This email is already in use';
-                    }
-                }
-                
-            }
+    return $code;
+}
 
-        if (empty($phone)) {
-            $err['phone'] = 'phone must be not empty';
+$verificationCode = generateVerificationCode();
+$_SESSION['verification'] = $verificationCode;
+
+
+
+
+
+if (isset($_POST['create'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $fullname = $_POST['fullname'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+    $err = [];
+
+
+
+    if (empty($username)) {
+        $err['username'] = 'username must not be empty';
+    } else {
+        if (!preg_match("/^[a-zA-Z0-9]{5,15}$/", $username)) {
+            $err['username'] = 'username must be from 5 to 25 characters';
         }
-        else {
-            if (!preg_match("/^[0-9]{10}$/", $phone)) {
-                $err['phone'] = 'phone must not be 10 numbers and not have characters';
+    }
+    if (empty($email)) {
+        $err['email'] = 'Email must not be empty';
+    } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $err['email'] = 'Invalid email';
+        } else {
+            // Kiểm tra email có tồn tại trong db chưa
+            $sql1 = "SELECT * FROM users WHERE email='$email' AND Type = 'user'";
+            $checkRegister = executeSingleResult($sql1);
+
+            if ($checkRegister) {
+                $err['email'] = 'This email is already in use';
             }
         }
-        if (empty($fullname)) {
-            $err['fullname'] = 'fullname must be not empty';
+    }
+
+    if (empty($phone)) {
+        $err['phone'] = 'phone must be not empty';
+    } else {
+        if (!preg_match("/^[0-9]{10}$/", $phone)) {
+            $err['phone'] = 'phone must not be 10 numbers and not have characters';
         }
-        else {
-            if (!preg_match("/^[a-zA-Z]{5,25}$/", $fullname)) {
-                $err['fullname'] = 'fullname must be from 5 to 25 characters and not a number';
-            }
+    }
+    if (empty($address)) {
+        $err['address'] = 'address must be not empty';
+    } else {
+        if (!preg_match("/^[a-zA-Z0-9 ]{1,60}$/", $address)) {
+            $err['address'] = 'Maximum length must not exceed 60 characters, including digits; minimum is 10 characters.';
         }
-        if (empty($password)) {
-            $err['password'] = 'password must be not empty';
-        } 
-        else{
-            if(!preg_match("/^[a-zA-Z0-9]{5,20}$/",$password)){
-                $err['password'] ='password must be from 5-10 characters and numbers';
-            }
+    }
+    if (empty($fullname)) {
+        $err['fullname'] = 'fullname must be not empty';
+    } else {
+        if (!preg_match("/^[a-zA-Z]{5,25}$/", $fullname)) {
+            $err['fullname'] = 'fullname must be from 5 to 25 characters and not a number';
         }
-        if (empty($cpassword)) {
-            $err['cpassword'] = 'cpassword must be not empty';
-        } 
-        else {
-            if ($cpassword!=$password) {
-                $err['cpassword'] = 'password and cpassword must be as same';
-            }
+    }
+    if (empty($password)) {
+        $err['password'] = 'password must be not empty';
+    } else {
+        if (!preg_match("/^[a-zA-Z0-9]{5,20}$/", $password)) {
+            $err['password'] = 'password must be from 5-10 characters and numbers';
         }
-    
-      
-        
-        // if (empty($birth)) {
-        //     $err['birth'] = 'birth must be not empty';
-        // } else {
-        //     $birthday_date = new DateTime($birth);
-    
-        //     $current_date = new DateTime();
-        //     $formatted_birthday = $birthday_date->format('m-d-Y');
-        //     $minimum_age_date = $current_date->sub(new DateInterval('P16Y'));
-            
-        //     if ($birthday_date > $minimum_age_date) {
-        //         $err['birth'] = 'You must be at least 16 years old to proceed!';
-        //     }
-        // }
-    
-        
-                if(empty($email)){
-                $err['email']='email must be not empty';
-                }else{
-                    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                        $err['email'] ='Invalid email';
-                    }else {
-                        //kiem tra email có tồn tại trong db chưa
-                        $sql1 = "SELECT * FROM users WHERE email='$email'";
-                        $checkRegister = executeSingleResult($sql1);
-                        if ($checkRegister) {
-                            $err['email'] = 'This email is already in use';
-                        }
-                    }
-                    
-                }
-              
-                    // var_dump(empty($err));
-                    // die();
-                    if(empty($err)){
-                        $pass = password_hash($password,PASSWORD_DEFAULT);
-                        $secretKey = 'your_secret_key';
-                        $expirationTimeMinutes = 60; // Thời gian hết hạn token: 60 phút
-                        $token = createToken($secretKey, $expirationTimeMinutes);
-                        $currentTime = time();
-     $sql = "INSERT INTO users(username,fullname,email,password) VALUES ('$username','$fullname','$email','$pass')";
+    }
+    if (empty($cpassword)) {
+        $err['cpassword'] = 'cpassword must be not empty';
+    } else {
+        if ($cpassword != $password) {
+            $err['cpassword'] = 'password and cpassword must be as same';
+        }
+    }
+
+
+
+    // if (empty($birth)) {
+    //     $err['birth'] = 'birth must be not empty';
+    // } else {
+    //     $birthday_date = new DateTime($birth);
+
+    //     $current_date = new DateTime();
+    //     $formatted_birthday = $birthday_date->format('m-d-Y');
+    //     $minimum_age_date = $current_date->sub(new DateInterval('P16Y'));
+
+    //     if ($birthday_date > $minimum_age_date) {
+    //         $err['birth'] = 'You must be at least 16 years old to proceed!';
+    //     }
+    // }
+
+
+
+    if (empty($err)) {
+        $pass = password_hash($password, PASSWORD_DEFAULT);
+
+
+
+        $sql = "INSERT INTO users(username,fullname,phone,email,status,password,Type,role) VALUES ('$username','$fullname','$phone','$email',2,'$pass','user',3)";
         execute($sql);
-  
-    $_SESSION['user'] = $checkUser;
-    header('location:index.php');
-    // $_SESSION['token'] = $token;
-    }
-    }
+        $_SESSION['email'] = $email;
 
-    ?>
+        $receiver = $email;
+        $subjects = "Your code to verify here:";
+        $messages = '<a href="http://localhost/techwiz/verificationPage.php?email=' . $email . '">Click me to verify</a>';
+        $senderEmail = 'davidphuc91@gmail.com';
+
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->SMTPAuth = true;
+        $mail->Username = 'davidphuc91@gmail.com';
+        $mail->Password = 'xmqclbncliprazpp';
+        $mail->setFrom($senderEmail);
+        $mail->addReplyTo($senderEmail);
+        $mail->addAddress($receiver);
+        $mail->Subject = $subjects;
+        $mail->msgHTML($messages);
+
+        if (!$mail->send()) {
+            $error = "Loi : " . $mail->ErrorInfo;
+            echo '<p>' . $error . '<p/>';
+        } else {
+            echo "<script>alert('Your mail has been sent,please check your mail to verify');
+                
+                </script>";
+        }
+    }
+    header('location:user-login.php');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -163,64 +183,73 @@
         }
     </style>
 </head>
+
 <body>
     <div class="wrapper">
         <form action="" method="post">
             <h1>Register</h1>
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['username'])) ? $err['username'] : ''; ?></span>
                 </div>
                 <input type="text" placeholder="Username" name="username" value="<?php echo isset($username) ? $username : ''; ?>">
                 <i class='bx bxs-user'></i>
             </div>
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['fullname'])) ? $err['fullname'] : ''; ?></span>
                 </div>
                 <input type="text" placeholder="Fullname" name="fullname" value="<?php echo isset($fullname) ? $fullname : ''; ?>">
-                <i class='bx bxs-lock-alt' ></i>
+                <i class='bx bxs-lock-alt'></i>
             </div>
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['email'])) ? $err['email'] : ''; ?></span>
                 </div>
                 <input type="text" placeholder="Email" name="email" value="<?php echo isset($email) ? $email : ''; ?>">
-                <i class='bx bxs-lock-alt' ></i>
+                <i class='bx bxs-lock-alt'></i>
             </div>
-            
+
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['phone'])) ? $err['phone'] : ''; ?></span>
                 </div>
                 <input type="text" placeholder="Phone" name="phone" value="<?php echo isset($phone) ? $phone : ''; ?>">
-                <i class='bx bxs-lock-alt' ></i>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <div class="input-box">
+                <div class="has-error">
+                    <span><?php echo (isset($err['address'])) ? $err['address'] : ''; ?></span>
+                </div>
+                <input type="text" placeholder="Address" name="address" value="<?php echo isset($address) ? $address : ''; ?>">
+                <i class='bx bxs-lock-alt'></i>
             </div>
 
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['password'])) ? $err['password'] : ''; ?></span>
                 </div>
                 <input type="password" placeholder="New Password" name="password" value="<?php echo isset($password) ? $password : ''; ?>">
-                <i class='bx bxs-lock-alt' ></i>
+                <i class='bx bxs-lock-alt'></i>
             </div>
 
             <div class="input-box">
-            <div class="has-error">
+                <div class="has-error">
                     <span><?php echo (isset($err['cpassword'])) ? $err['cpassword'] : ''; ?></span>
                 </div>
                 <input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo isset($cpassword) ? $cpassword : ''; ?>">
-                <i class='bx bxs-lock-alt' ></i>
+                <i class='bx bxs-lock-alt'></i>
             </div>
 
             <!-- <div class="remember-forgot">
                 <label><input type="checkbox">Remember me</label>
                 <a href="#">Forgot password</a>
             </div> -->
-<br><br>
+            <br><br>
             <button type="submit" class="btn" name="create">Create new account</button>
 
         </form>
     </div>
 </body>
+
 </html>
