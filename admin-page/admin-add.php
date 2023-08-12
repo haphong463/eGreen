0643423f9719bf944ffd95da51a0dc4825317bef
Biden678 <davidphuc91@gmail.com>
2023-08-12 23:08:32 +0700

    Update admin-add.php

--- admin-page/admin-add.php
+++ admin-page/admin-add.php
@@ -11,34 +11,47 @@ if (isset($_POST['add'])) {
     $password = $_POST['password'];
     $role = $_POST['role'];
     $err = [];
-    $pass = password_hash($password, PASSWORD_DEFAULT);
+    
 
-    // Tạo mã token
-    function createToken($secretKey, $expirationTimeMinutes)
-    {
-        // Lấy thời gian hiện tại
-        $currentTime = time();
-        // Tính thời gian hết hạn
-        $expirationTime = $currentTime + ($expirationTimeMinutes * 60);
+    if(empty($email)){
+        $err['email']='email must be not empty';
+        }else{
+            if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                $err['email'] ='Invalid email';
+            }else {
+                //kiem tra email có tồn tại trong db chưa
+                $sql1 = "SELECT * FROM users WHERE email='$email'";
+                $checkRegister = executeSingleResult($sql1);
+                if ($checkRegister) {
+                    $err['email'] = 'This email is already in use';
+                }
+            }
+        }
 
-        // Tạo payload (dữ liệu chứa trong token)
-        $payload = array(
-            'user_id' => '1234567890',  // Thông tin cần lưu trữ trong token
-            'exp' => $expirationTime  // Thời gian hết hạn của token
-        );
-
-        // Tạo mã token với secret key
-        $jwtBuilder = new JWT();
-        $token = $jwtBuilder->encode($payload, $secretKey, 'HS256');
-
-        return $token;
+     if (empty($phone)) {
+        $err['phone'] = 'phone must be not empty';
+    } else {
+        if (!preg_match("/^[0-9]{10}$/", $phone)) {
+            $err['phone'] = 'phone must not be 10 numbers and not have characters';
+        }
     }
-    $secretKey = 'your_secret_key';
-    $expirationTimeMinutes = 60; // Thời gian hết hạn token: 60 phút
-    $token = createToken($secretKey, $expirationTimeMinutes);
-    $sql = "INSERT INTO users (email,password,phone,role,token) VALUES ('$email','$pass','$phone','$role','$token')";
-    execute($sql);
-    header('location:admin.php');
+    if(empty($password)){
+        $err['password']='password must be not empty';
+        }else{
+            if(!preg_match("/^[a-zA-Z0-9]{5,20}$/",$password)){
+                $err['password'] ='password must be from 5-10 characters and numbers';
+            }
+        }
+
+        if(empty($err) ){
+            $pass = password_hash($password,PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (email,password,phone,role) VALUES ('$email','$pass','$phone','$role')";
+            execute($sql);
+            header('location:admin.php');
+}
+
+    
+ 
 }
 
 ?>
@@ -54,6 +67,11 @@ if (isset($_POST['add'])) {
     <title>Admin Dashboard Panel</title>
     <link rel="stylesheet" href="admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        .has-error {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
@@ -72,16 +90,26 @@ if (isset($_POST['add'])) {
             <div class="product-physical">
                 <form action="" method="post">
                     <div class="form-group">
+                    <div class="has-error">
+                                        <span><?php echo (isset($err['email'])) ? $err['email'] : ''; ?></span>
+                                    </div>
                         <label for="name">Email :</label>
-                        <input id="name" class="form-control" type="text" name="email">
+                        <input id="name" class="form-control" type="text" name="email" value="<?php echo isset($email) ? $email : ''; ?>">
                     </div>
+
                     <div class="form-group">
+                    <div class="has-error">
+                                        <span><?php echo (isset($err['password'])) ? $err['password'] : ''; ?></span>
+                                    </div>
                         <label for="name">Password :</label>
-                        <input id="name" class="form-control" type="text" name="password">
+                        <input id="name" class="form-control" type="password" name="password" value="<?php echo isset($password) ? $password : ''; ?>">
                     </div>
                     <div class="form-group">
+                    <div class="has-error">
+                                        <span><?php echo (isset($err['phone'])) ? $err['phone'] : ''; ?></span>
+                                    </div>
                         <label for="name">Phone :</label>
-                        <input id="name" class="form-control" type="text" name="phone">
+                        <input id="name" class="form-control" type="text" name="phone" value="<?php echo isset($phone) ? $phone : ''; ?>">
                     </div>
                     <div class="form-group">
                         <label for="name">Role :</label>
